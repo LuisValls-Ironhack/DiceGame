27b5f1376afd8ab7e55b5a34879c2f2e7b5a416d
LuisValls-Ironhack <84102901+LuisValls-Ironhack@users.noreply.github.com>
2021-06-14 12:29:26 +0200

    Starting files
    These are the initial files.

---
+++ Project X Folder 0/index.js
@@ -0,0 +1,26 @@
+	// Player name
+	var player1 = "Player 1";
+	var player2 = "Player 2";
+
+	// Function to change the player name
+	function editNames() {
+		player1 = prompt("Change Player1 name");
+		
+
+		document.querySelector("p.Player1").innerHTML = player1;
+	
+	}
+
+	// Function to roll the dice
+	function rollTheDice() {
+		setTimeout(function () {
+			var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+			var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+		
+			
+			}
+		,)};
+	
+
+
